02de5cff43dfa3f3d24f8868aff10a1638d3f7e4
Thomas Capelle <tcapelle@pm.me>
2022-04-22 20:16:41 +0200

    2 models

--- app.py
+++ app.py
@@ -9,13 +9,20 @@ import streamlit as st
 DEFAULT_TEXT = """Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002."""
 
 
-
 st.title("NER using spaCy")
 
 # key = st.text_input("Paste your wandb API key here https://wandb.ai/authorize")
 # wandb.login()
 
 wandb.login(anonymous="must")
+_models = ["en_core_web_sm", "en_core_web_md"]
+
+models = []
+for model in _models:
+    artifact = wandb.Api().artifact(f'capecape/st30/{model}:v0', type='model')
+    models.append(artifact.download())
+
+model = st.selectbox("Select your spaCy model artifact", models)
 
 artifact = wandb.Api().artifact('capecape/st30/spacy_model:v0', type='model')
 model = artifact.download()
