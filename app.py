3ea09e0599191af5a72fa08d14e6880036c4b162
Thomas Capelle <tcapelle@pm.me>
2022-04-22 19:48:46 +0200

    pull model from artifact

--- app.py
+++ app.py
@@ -3,16 +3,22 @@ Example using the components provided by spacy-streamlit in an existing app.
 Prerequisites:
 python -m spacy download en_core_web_sm
 """
-import spacy_streamlit
+import spacy_streamlit, wandb
 import streamlit as st
 
 DEFAULT_TEXT = """Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002."""
 
-spacy_model = "en_core_web_sm"
+
 
 st.title("NER using spaCy")
+
+
+artifact = wandb.Api().artifact('capecape/st30/spacy_model:v0', type='model')
+model = artifact.download()
+
+
 text = st.text_area("Text to analyze", DEFAULT_TEXT, height=200)
-doc = spacy_streamlit.process_text(spacy_model, text)
+doc = spacy_streamlit.process_text(model, text)
 
 ner_labels = ["CARDINAL", "DATE", "EVENT", "FAC", "GPE", "LANGUAGE", 
               "LAW", "LOC", "MONEY", "NORP", "ORDINAL", "ORG", "PERCENT", 
@@ -24,4 +30,4 @@ spacy_streamlit.visualize_ner(
     show_table=False,
     title="Persons, dates and locations",
 )
-st.text(f"Analyzed using spaCy model {spacy_model}")
+st.text(f"Analyzed using spaCy model {model}")
